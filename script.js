6b14b629650d50827475f1936e264c161cd512c4
rajsadian <rajsadian@gmail.com>
2024-04-25 17:15:01 +0700

    building pyramid use \n in looping javascript

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 const character = "#";
 const count = 8;
 const rows = [];
+
 for (let i = 0; i < count; i = i + 1) {
-  console.log(rows);
+  rows.push(i);
 }
+
+let result = "";
+
+for (const row of rows) {
+  result = result + row;
+  result = result + "\n" + row;
+}
+console.log(result);
