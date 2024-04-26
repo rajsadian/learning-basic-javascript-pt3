7e2080fb78bc9188cfcacc33c065f5bb50b25148
rajsadian <rajsadian@gmail.com>
2024-04-27 03:04:32 +0700

    building pyramid to upside down

--- script.js
+++ script.js
@@ -10,8 +10,17 @@ function padRow(rowNumber, rowCount) {
   );
 }
 
-for (let i = 0; i < count; i = i + 1) {
-  rows.push(padRow(i + 1, count));
+// TODO: use a different type of loop
+/*for (let i = 1; i <= count; i++) {
+  rows.push(padRow(i, count));
+}*/
+
+/*while (rows.length < count) {
+  rows.push(padRow(rows.length + 1, count));
+}*/
+
+for (let i = count; i > 0; i = i - 1) {
+  rows.push(padRow(i, count));
 }
 
 let result = "";
