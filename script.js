8187a58d6d8aca7685b37bba9548590aff13be86
rajsadian <rajsadian@gmail.com>
2024-04-27 04:44:42 +0700

    complete building pyramid

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const character = "#";
 const count = 8;
 const rows = [];
+let inverted = true;
 
 function padRow(rowNumber, rowCount) {
   return (
@@ -10,18 +11,21 @@ function padRow(rowNumber, rowCount) {
   );
 }
 
-// TODO: use a different type of loop
 for (let i = 1; i <= count; i++) {
-  rows.push(padRow(i, count));
+  if (inverted) {
+    rows.unshift(padRow(i, count));
+  } else {
+    rows.push(padRow(i, count));
+  }
 }
 
-/*while (rows.length < count) {
+while (rows.length < count) {
   rows.push(padRow(rows.length + 1, count));
-}*/
+}
 
-/*for (let i = count; i > 0; i--) {
+for (let i = count; i > 0; i--) {
   rows.push(padRow(i, count));
-}*/
+}
 
 let result = "";
 
