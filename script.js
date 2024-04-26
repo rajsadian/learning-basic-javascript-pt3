c02be5fb331864335b4a389f6e5ae80d583843ae
rajsadian <rajsadian@gmail.com>
2024-04-27 04:33:37 +0700

    using .unshift() .push()

--- script.js
+++ script.js
@@ -19,9 +19,15 @@ function padRow(rowNumber, rowCount) {
   rows.push(padRow(rows.length + 1, count));
 }*/
 
-for (let i = count; i > 0; i = i - 1) {
+/*for (let i = count; i > 0; i--) {
   rows.push(padRow(i, count));
-}
+}*/
+
+const numbers = [1, 2, 3];
+
+const unshifted = numbers.unshift(5);
+console.log(unshifted);
+console.log(numbers);
 
 let result = "";
 
